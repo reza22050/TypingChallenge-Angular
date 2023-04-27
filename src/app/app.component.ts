0871e6503dd4c5fcc5f69b9722f6a87526bf02a0
Reza <RezaTaghipour99@gmail.com>
2023-04-27 22:55:24 +0430

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { ElementSchemaRegistry } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typing';
+  randomText: string = faker.lorem.sentence();
+  inputText: string = '';
+
+  onChangeInput(text: string){
+    this.inputText = text;      
+  }
+
+  compare(letterRandom:string, letterEnter: string): string{
+    if(!letterEnter)
+      return 'pending'
+    return letterRandom === letterEnter ? 'correct' : 'incorect';
+  }
 }
